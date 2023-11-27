85a79ae56e0f70b67d4db4b293d10c8ba77a6ae5
naifhogo <125391781+naifhogo@users.noreply.github.com>
2023-11-27 20:44:40 +0300

    BE_3-Exercises_4

--- express-app/app.js
+++ express-app/app.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const app = express();
+app.get('/', (req, res) => {
+  res.send('Welcome to the homepage!');
+});
+app.post('/submit', (req, res) => {
+  // Process the submitted form data here
+  // ...
 
-//route files:
+  // Send a response back to the client
+  res.send('Form submitted successfully!');
+});
+
+app.use(express.urlencoded({ extended: true }));
 const homeRouter = require('./routes/homeRouter');
 const userRouter = require('./routes/userRouter');
 
-//routers:
+
+// Use the formRoutes middleware
+
 app.use('/', homeRouter);
 app.use('/users', userRouter);
-
-
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+app.use(express.static('swe-363-231'));
--- express-app/routes/homeRouter.js
+++ express-app/routes/homeRouter.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.get('/', (req, res) => {
   res.send('Welcome to the homepage!');
 });
 
 router.get('/about', (req, res) => {
   res.send('About page');
+});
+
+
+router.post('/submit', (req, res) => {
+  const formData = req.body;
+
+  res.send('Form submitted successfully!');
 });
 
 module.exports = router;
